0962221f0aa2ac059a5dbeb6a2d6895089d743f2
asharmehmood <30666174+asharmehmood@users.noreply.github.com>
2017-08-09 04:52:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def recommendationalgo():
 	
 	#s_id='1C11'
 	#row_title=['5 Marla House for Sale in Islamabad E-11/4','571 Square Feet Apartment for Sale in Islamabad Gulberg Greens','2 Kanal Residential Land for Sale in Islamabad Bahria Enclave','88 Marla House for Sale in Islamabad Sector G-6','10 Marla House for Sale in Islamabad E-11/2']
-	
+	stg='nothing'
 	cominglist={}
 	comingdata={}
 
@@ -277,8 +277,8 @@ def recommendationalgo():
 	
 	for val in suggestiondic:
 		if val==s_id:
-			str=suggestiondic[val]
-	return str
+			stg=suggestiondic[val]
+	return stg
 
 #Price
 def convertMaximum(pric, unit):

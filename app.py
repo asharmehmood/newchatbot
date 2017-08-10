7dadea1300cb32b77055c276d34b09b6c4db4069
asharmehmood <30666174+asharmehmood@users.noreply.github.com>
2017-08-10 10:06:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -333,9 +333,9 @@ def makeWebhookResult(data):
 		row_number[i]=data[i]['number']
 		row_image[i]=data[i]['image']
 		row_city[i]=data[i]['city_name']
-		speech_data_parts="Here is record " + suggesting_user + str(i+1) +":"+ row_title[i]+" in city "+row_city[i] + " price is "+ str(row_price[i]) + "."
+		speech_data_parts="Here is record " + str(i+1) +":"+ row_title[i]+" in city "+row_city[i] + " price is "+ str(row_price[i]) + "."
 		speech_data = speech_data + speech_data_parts
-		text_data_parts ="Here is record " + suggesting_user + str(i+1) +":"+ row_title[i]+" in city "+row_city[i] + " price is "+ str(row_price[i])+ ". For Info about this contact at number "+str(row_number[i]) + "."
+		text_data_parts ="Here is record " + str(i+1) +":"+ row_title[i]+" in city "+row_city[i] + " price is "+ str(row_price[i])+ ". For Info about this contact at number "+str(row_number[i]) + "."
 		text_data = text_data + text_data_parts	
 		i+=1
 	print(row_title[0])

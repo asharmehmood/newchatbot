194d719d0cc8b713067cc0d4f7eb77234935979c
asharmehmood <30666174+asharmehmood@users.noreply.github.com>
2017-08-10 16:38:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -212,7 +212,7 @@ def recommendationalgo():
 				if housekey == value:
 					cityy=key
 			if(housecount>hcountf and cityy==city_names):
-				print (hfh)
+				print ("andr agya house me :", hfh)
 				hcountf=housecount
 				hfh=house
 			housecount=0
